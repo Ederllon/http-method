3aebc52e190a9bdcf4c130676dd1400b2ff67ddf
Ederllon <156702239+Ederllon@users.noreply.github.com>
2024-06-11 23:08:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 var http = require('http');
-http.createServer(
-  function(req,res){
-    res.end('On Page')
-  }
-).listen(808);
+http.createServer(function(req,res){ res.end('On Page')}).listen(808);
 console.log('ONLINE!');
